ca54cab7b4d75266a848edbd8fe1198504cd256f
virtualfinance1992 <virtualfinanceservice@gmail.com>
2025-04-21 19:05:43 +0530

    🔥 Fix DATABASES config for Render PostgreSQL

--- backend/virtual_finance/settings.py
+++ backend/virtual_finance/settings.py
@@ -137,16 +137,16 @@ WSGI_APPLICATION = 'virtual_finance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Virtual-Finance',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'Virtual-Finance',
+    #    'USER': 'postgres',
+     #   'PASSWORD': '123456',
+     #   'HOST': 'localhost',
+      #  'PORT': '5432',
+    #}
+#}
 
 
 
