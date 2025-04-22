45a0a15ee951b1683b5092f2f50c30370d4de676
virtualfinance1992 <virtualfinanceservice@gmail.com>
2025-04-22 13:13:40 +0530

    ✅ Rename setupadmin to create_admin

--- backend/user_mgmt/management/commands/create_admin.py
+++ backend/user_mgmt/management/commands/create_admin.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Create a default admin user if one does not exist'
+    help = 'Create an admin user for deployment'
 
     def handle(self, *args, **kwargs):
+        call_command('migrate', interactive=False)
         User = get_user_model()
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
-            self.stdout.write(self.style.SUCCESS('✅ Admin user created'))
+            self.stdout.write(self.style.SUCCESS("✅ Admin user created."))
         else:
-            self.stdout.write(self.style.WARNING('ℹ️ Admin user already exists'))
+            self.stdout.write(self.style.WARNING("ℹ️ Admin already exists."))
