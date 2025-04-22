13d4611835a45a99e73b057c9703abb51da8efe5
virtualfinance1992 <virtualfinanceservice@gmail.com>
2025-04-22 12:39:58 +0530

    ✅ Add setupadmin command to create default superuser

--- backend/user_mgmt/management/commands/setupadmin.py
+++ backend/user_mgmt/management/commands/setupadmin.py
@@ -1,16 +1,11 @@
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Run migrations and create initial admin user'
-
-    def handle(self, *args, **options):
-        call_command('migrate', interactive=False)
-
+    def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(username='admin', email='admin@example.com', password='admin1234')
-            self.stdout.write(self.style.SUCCESS('✅ Admin user created.'))
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(username="admin", password="admin1234")
+            self.stdout.write(self.style.SUCCESS("✅ Superuser 'admin' created"))
         else:
-            self.stdout.write(self.style.WARNING('⚠️ Admin user already exists.'))
+            self.stdout.write("ℹ️ Admin already exists.")
