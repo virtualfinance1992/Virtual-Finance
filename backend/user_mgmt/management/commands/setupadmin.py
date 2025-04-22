fc1e050a6214a731a4c45006e223dbcba128cf0e
virtualfinance1992 <virtualfinanceservice@gmail.com>
2025-04-22 12:51:26 +0530

    ✅ Fix setupadmin command and register properly

--- backend/user_mgmt/management/commands/setupadmin.py
+++ backend/user_mgmt/management/commands/setupadmin.py
@@ -2,12 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Create a default admin user if not exists'
+    help = 'Create a default admin user if one does not exist'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin1234")
-            self.stdout.write(self.style.SUCCESS("✅ Admin user created"))
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+            self.stdout.write(self.style.SUCCESS('✅ Admin user created'))
         else:
-            self.stdout.write(self.style.WARNING("ℹ️ Admin already exists"))
+            self.stdout.write(self.style.WARNING('ℹ️ Admin user already exists'))
